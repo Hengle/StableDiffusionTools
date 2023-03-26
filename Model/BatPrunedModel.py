0bb9288a9ab9f560e423178842b120dbdc0f5370
ShiyumeMeguri <102138304+ShiyumeMeguri@users.noreply.github.com>
2023-03-27 07:05:24 +0800

    输出一个就够了

--- Model/BatPrunedModel.py
+++ Model/BatPrunedModel.py
@@ -2,20 +2,23 @@ import os
 import argparse
 
 def generate_bat_files(path, weight):
+    commands = []
     for root, dirs, files in os.walk(".", topdown=False):
         for name in files:
             if name.endswith(".ckpt") or name.endswith(".safetensors"):
                 if path in name:
                     # Generate the command to be written in the bat file
-                    cmd = f"python LoRA_Pruneder.py {name} {name[:-5]}_Pruned {weight}"
-                    # Write the command to a bat file
-                    with open(f"{name[:-5]}.bat", "w") as bat_file:
-                        bat_file.write(cmd)
-                    print(f"{name[:-5]}.bat generated!")
+                    cmd = f"LoRA_Pruneder.py {name} {name[:-5]}_Pruned {weight}"
+                    commands.append(cmd)
+                    print(f"{name[:-5]} command generated!")
+    # Write all commands to a bat file
+    with open("run_pruning.bat", "w") as bat_file:
+        bat_file.write("\n".join(commands))
+    print("run_pruning.bat generated!")
 
 def main(args):
     # Call the function with the search string specified in the command line argument
-    generate_bat_files(args.path,args.weight)
+    generate_bat_files(args.path, args.weight)
 
 if __name__ == "__main__":
     # Parse the command line arguments
